94facf32b112439906a37efd91dd44794d1fc13b
Joe Zidell <joe.zidell@gmail.com>
2022-09-16 17:29:54 -0400

    wip: commander

--- index.ts
+++ index.ts
@@ -8,8 +8,68 @@ import { simplifyTree } from "./parser/simplifier";
 // import SyntaxTreeGenerator from './syntax/generator';
 // import TranspilerToGo from './transpilers/to/go';
 // import TranspilerToTypescript from './transpilers/to/ts';
+import { program } from 'commander';
 
 void (async (): Promise<void> => {
+	program
+		.command('lexify')
+		.description('Split your .joe code into tokens')
+		.argument('<code>', 'code to lexify')
+		.option('-v, --verbose', 'display debugging information')
+		.option('-o, --output', 'file path to send the output to')
+		.action(async (sourceCode, options, command) => {
+			// console.debug({sourceCode, options, command});
+
+			try {
+				const tokens = new Lexer(sourceCode).lexify();
+
+				// filename is 3rd arg
+				if (options.output) {
+					await fs.writeFile(options.output, JSON.stringify(tokens, undefined, '\t'));
+				} else {
+					console.table(tokens);
+				}
+			} catch (e) {
+				const error = e as LexerError;
+
+				console.log(`Error: ${error.message}`);
+				if (typeof error.getTokens === 'function') {
+					console.debug('Extracted tokens:');
+					console.table(error.getTokens());
+				}
+
+				console.log('Stack Trace:');
+				console.error(error.stack);
+			}
+		});
+
+	program
+		.command('parse')
+		.description('Parse your .joe code into a Parse Tree')
+		.argument('<code>', 'code to parse')
+		.option('-v, --verbose', 'display debugging information')
+		.option('-o, --output', 'file path to send the output to');
+
+	program
+		.command('syntax')
+		.description('Generate an Abstract Syntax Tree (AST) from your .joe code')
+		.argument('<code>', 'code')
+		.option('-v, --verbose', 'display debugging information')
+		.option('-o, --output', 'file path to send the output to');
+
+	program
+		.command('transpile')
+		.description('Transpile your .joe code into another language')
+		.argument('<lang>', 'language')
+		.argument('<code>', 'code')
+		.option('-v, --verbose', 'display debugging information', false)
+		.option('-o, --output <filename>', 'file path to send the output to');
+
+	program.parse();
+	const options = program.opts();
+	console.debug({options});
+	return;
+
 	const command = process.argv[2];
 
 	switch (command) {
@@ -64,28 +124,28 @@ void (async (): Promise<void> => {
 			}
 			break;
 
-			case 'syntax':
-				try {
-					const [, , , sourceCode, outputFile] = process.argv;
+		case 'syntax':
+			try {
+				const [, , , sourceCode, outputFile] = process.argv;
 
-					// const syntaxTree = new SyntaxTreeGenerator(new Parser(new Lexer(sourceCode).lexify()).parse()).generate();
+				// const syntaxTree = new SyntaxTreeGenerator(new Parser(new Lexer(sourceCode).lexify()).parse()).generate();
 
-					// if (outputFile) {
-					// 	await fs.writeFile(outputFile, inspect(syntaxTree, { showHidden: true, depth: null }));
-					// } else {
-					// 	console.debug(inspect(syntaxTree, { showHidden: true, depth: null }));
-					// }
-				} catch (e) {
-					const error = e as ParserError;
+				// if (outputFile) {
+				// 	await fs.writeFile(outputFile, inspect(syntaxTree, { showHidden: true, depth: null }));
+				// } else {
+				// 	console.debug(inspect(syntaxTree, { showHidden: true, depth: null }));
+				// }
+			} catch (e) {
+				const error = e as ParserError;
 
-					console.log(`Error: ${error.message}`);
-					console.debug('Derived CST:');
-					console.debug(error.getTree());
+				console.log(`Error: ${error.message}`);
+				console.debug('Derived CST:');
+				console.debug(error.getTree());
 
-					console.log('Stack Trace:');
-					console.error(error.stack);
-				}
-				break;
+				console.log('Stack Trace:');
+				console.error(error.stack);
+			}
+			break;
 
 		case 'transpile':
 			try {
