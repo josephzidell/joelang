1efe861d7ef25c60a8c1977d7595c603c0083d98
Joe Zidell <joe.zidell@gmail.com>
2023-05-04 20:57:12 -0400

    Addresses some feedback from #24

--- .bin/joec.bat
+++ .bin/joec.bat
@@ -1,3 +1,12 @@
 @echo off
 
-node _build/compile.js %*
+rem Get the directory of the current script
+set "SCRIPT_DIR=%~dp0"
+
+rem Remove trailing backslash from the script directory
+set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
+
+rem Reference a file in the parent directory of the script
+for %%i in ("%SCRIPT_DIR%\..") do set "PARENT_DIR=%%~dpi"
+
+node %PARENT_DIR%_build/compile.js %*
