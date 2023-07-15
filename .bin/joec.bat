1c2565b54aeec22da3ea8fb838234a7a94a08c95
person93 <mtepfer1@gmail.com>
2023-07-15 07:02:14 -0400

    Run development compiler in source tree (#32)
    * Simplifies scripts in .bin directory
    
    * Adds package script to run the compiler
    
    ---------
    
    Co-authored-by: Joseph Zidell <josephzidell@users.noreply.github.com>

--- .bin/joec.bat
+++ .bin/joec.bat
@@ -3,10 +3,4 @@
 rem Get the directory of the current script
 set "SCRIPT_DIR=%~dp0"
 
-rem Remove trailing backslash from the script directory
-set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
-
-rem Reference a file in the parent directory of the script
-for %%i in ("%SCRIPT_DIR%\..") do set "PARENT_DIR=%%~dpi"
-
-node %PARENT_DIR%_build/compile.js %*
+node %SCRIPT_DIR%/joec %*
